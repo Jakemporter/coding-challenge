794eabbb06272e565efce86b72c32aa86ddc43a7
Jake Porter <Jake.m.porter@gmail.com>
2023-06-27 22:07:18 -0500

    coding challenge

---
+++ Coding-Challenge.js
@@ -0,0 +1,20 @@
+// Challenge: Palindrome Checker
+// Overview:
+// A palindrome is a word, phrase, number, or other sequences of characters that reads the same forward and backward, ignoring spaces, punctuation, and capitalization.
+
+// The objective of this coding challenge is to create a JavaScript function named isPalindrome that checks if a provided string is a palindrome or not.
+
+// Challenge Details:
+// Create a JavaScript function named isPalindrome.
+// This function should take one argument, a string.
+// The function should return true if the string is a palindrome.
+// The function should return false if the string is not a palindrome.
+// The function should ignore spaces, punctuation, and capitalization when checking for a palindrome.
+// Example:
+// Input: isPalindrome("racecar")
+
+// Output: true
+
+// Input: isPalindrome("JavaScript")
+
+// Output: false
